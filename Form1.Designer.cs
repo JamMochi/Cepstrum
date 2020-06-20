12d409e4e68f96a6edb290c7bdd8503fd3f5b424
MochiJammy <ngh2@unlv.nevada.edu>
2020-06-19 20:08:56 -0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace CepProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.sigChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.sourceList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Play = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sigChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Record";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Record_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(172, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 46);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Next Window";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.CreateWindow_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(172, 65);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(154, 46);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Display";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Display_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(608, 13);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(154, 46);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Refresh";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(768, 13);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(154, 46);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // sigChart
+            // 
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.White;
+            chartArea1.Name = "ChartArea1";
+            this.sigChart.ChartAreas.Add(chartArea1);
+            this.sigChart.Location = new System.Drawing.Point(12, 168);
+            this.sigChart.Name = "sigChart";
+            this.sigChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SemiTransparent;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            this.sigChart.Series.Add(series1);
+            this.sigChart.Size = new System.Drawing.Size(910, 430);
+            this.sigChart.TabIndex = 6;
+            this.sigChart.Text = "chart1";
+            // 
+            // sourceList
+            // 
+            this.sourceList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.sourceList.HideSelection = false;
+            this.sourceList.Location = new System.Drawing.Point(608, 65);
+            this.sourceList.Name = "sourceList";
+            this.sourceList.Size = new System.Drawing.Size(314, 97);
+            this.sourceList.TabIndex = 7;
+            this.sourceList.UseCompatibleStateImageBehavior = false;
+            this.sourceList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Device";
+            this.columnHeader1.Width = 174;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Source";
+            this.columnHeader2.Width = 172;
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(12, 117);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(154, 46);
+            this.Play.TabIndex = 8;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 610);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.sourceList);
+            this.Controls.Add(this.sigChart);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Sig";
+            ((System.ComponentModel.ISupportInitialize)(this.sigChart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataVisualization.Charting.Chart sigChart;
+        private System.Windows.Forms.ListView sourceList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button Play;
+    }
+}
+
